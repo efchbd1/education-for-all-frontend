987e0b88d5e835ea5090c78c61cc9b4e46047dd4
efchbd1013 <efchbd1013@gmail.com>
2025-05-05 11:00:18 +0300

    Changing API_BASE_URL

--- src/data/services/config/api.ts
+++ src/data/services/config/api.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL!;
+// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL!;
+export const API_BASE_URL = "https://education-for-all-frontend.onrender.com/api";
+
