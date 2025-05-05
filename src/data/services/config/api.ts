28f3150cecdf049d3a9f93d335b2fce93ebacb46
efchbd1013 <efchbd1013@gmail.com>
2025-05-05 11:16:17 +0300

    Fixing API_BASE_URL

--- src/data/services/config/api.ts
+++ src/data/services/config/api.ts
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL!;
-export const API_BASE_URL = "https://education-for-all-frontend.onrender.com/api";
+export const API_BASE_URL = "https://education-for-all-backend.onrender.com/api";
 
